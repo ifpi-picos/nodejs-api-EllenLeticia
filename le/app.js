b85489af98435f20392874d0d4052d7ab3895702
EllenLeticia <leticiaellenleal@gmail.com>
2019-11-05 15:29:08 -0300

    atvcliente

--- le/app.js
+++ le/app.js
@@ -2,10 +2,14 @@ const express = require('express');
 const routers = require('./routes')
 const conectaMongodb = require('./config/mongodb');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 
 const app = express();
-app.use(bodyParser.json());                                                                                                                                                                                                                                                                                                                      
+app.use(cors());
+app.use(bodyParser.json());
+
 app.use('/', routers);
+
 conectaMongodb();
 
 app.listen(3000, () => console.log('App Online'));
